40d02e4c6766b1783d09bdc4906a7fb1db28ac55
dbettini <dbettini91@gmail.com>
2016-12-11 15:44:16 +0100

    Create random name generator api

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const moniker = require("moniker");
 
 app.use(express.static("public"));
 
+app.get("/name", (req, res) => {
+    res.send({ name: moniker.choose() });
+});
 app.get("*", (req, res) => {
     res.sendFile("/public/index.html");
 });
